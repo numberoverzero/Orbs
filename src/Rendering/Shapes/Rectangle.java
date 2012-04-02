368057040e3b559cefbc69ecfabea1baabb7c1ec
Joseph Cross <joe.mcross@gmail.com>
2012-04-01 23:31:43 -0400

    Re-wrote the DrawOutline method of Rendering\Shapes.Rectangle so that we stopped generating 4 new Vec2s each iteration.  I haven't performance tested it yet, and while it's silly to do optimization now, I'm in a car and it's only about a 30min ride, so I figure this is the right time for easy changes.

--- src/Rendering/Shapes/Rectangle.java
+++ src/Rendering/Shapes/Rectangle.java
@@ -29,12 +29,49 @@ public final class Rectangle {
     }
 
     public static void Draw(SpriteBatch batch, Vec2 pos, Vec2 dimensions, double rot, Color color) {
-        Vec2 origin = dimensions.DividedBy(2);
         batch.setColor(color);
-        batch.draw(texture, pos.X - origin.X, pos.Y - origin.Y, origin.X, origin.Y, dimensions.X, dimensions.Y,
-                1, 1, Util.ToDegrees(rot), 0, 0, 1, 1, false, false);
+        batch.draw(texture, pos.X - dimensions.X / 2, pos.Y - dimensions.Y / 2, dimensions.X / 2, dimensions.Y / 2,
+                dimensions.X, dimensions.Y, 1, 1, Util.ToDegrees(rot), 0, 0, 1, 1, false, false);
     }
 
+    public static void DrawOutline(SpriteBatch batch, Vec2 pos, Vec2 dimensions,
+                                   double rot, Color color, float lineWidth) {
+        batch.setColor(color);
+
+        double dim2x = dimensions.X / 2;
+        double dim2y = dimensions.Y / 2;
+        // Here dim2x/y are the half dimensions of the rectangle
+        double[] corners = new double[]{
+                -dim2x + lineWidth, -dim2y + lineWidth,
+                -dim2x + lineWidth, dim2y - lineWidth,
+                dim2x - lineWidth, dim2y - lineWidth,
+                dim2x - lineWidth, -dim2y + lineWidth,
+                -dim2x + lineWidth, -dim2y + lineWidth};
+
+        // Here dim2x/y are the rotated x/y positions of the corner being rotated
+        final double st = Math.sin(rot);
+        final double ct = Math.cos(rot);
+        for (int i = 0; i < 10; i += 2) {
+            dim2x = (ct * corners[i]) - (st * corners[i + 1]);
+            dim2y = (st * corners[i]) + (ct * corners[i + 1]);
+            corners[i] = dim2x + pos.X;
+            corners[i + 1] = dim2y + pos.Y;
+        }
+
+        // Here dim2x/y are the delta x/y values between this corner and the next
+        float scaleX;
+        double segRot;
+        for (int i = 0; i < 8; i += 2) {
+            dim2y = corners[i + 3] - corners[i + 1];
+            dim2x = corners[i + 2] - corners[i];
+            scaleX = Util.Distance(dim2x, dim2y) + lineWidth;
+            segRot = Util.Angle(dim2y, dim2x);
+            batch.draw(texture, (float) corners[i], (float) corners[i + 1], 0, 0, scaleX, lineWidth,
+                    1, 1, Util.ToDegrees(segRot), 0, 0, 1, 1, false, false);
+        }
+    }
+
+    /*// Old draw method using Vec2s, lots of allocation.
     public static void DrawOutline(SpriteBatch batch, Vec2 pos, Vec2 dimensions, double rot, Color color, float lineWidth) {
         batch.setColor(color);
         Vec2 dim2 = dimensions.DividedBy(2);
@@ -50,8 +87,7 @@ public final class Rectangle {
             corners[i].Add(pos);
         }
 
-        Vec2 scale = new Vec2(0, lineWidth);
-        Vec2 segment;
+        Vec2 segment, scale = new Vec2(0, lineWidth);
         float lineRot;
         for (int i = 0; i < 4; i++) {
             segment = (corners[i + 1].Minus(corners[i]));
@@ -59,7 +95,7 @@ public final class Rectangle {
             lineRot = (float) segment.Angle();
             batch.draw(texture, corners[i].X, corners[i].Y, 0, 0, scale.X, scale.Y,
                     1, 1, Util.ToDegrees(lineRot), 0, 0, 1, 1, false, false);
-            //Draw(batch, corners[i], Vec2.Zero(), scale, segment.Angle(), color);
         }
     }
+    */
 }
